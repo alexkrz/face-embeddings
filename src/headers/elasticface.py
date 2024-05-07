01258fbf72b9cfe74d27e9158401063698625b25
Alexander Kurz <alex.kurz95@gmail.com>
2024-05-07 16:34:35 +0200

    Compute flops in iresnet backbone

---
+++ src/headers/elasticface.py
@@ -0,0 +1,13 @@
+from .arcmargin import ArcMarginHeader
+
+
+class ElasticFaceHeader(ArcMarginHeader):
+    """
+    ElasitcFaceHeader class
+    Reference: https://ieeexplore.ieee.org/document/9857083
+    """
+
+    # TODO: Add ElasticFace Implementation
+
+    def __init__(self, in_features, out_features):
+        super().__init__(in_features=in_features, out_features=out_features)
