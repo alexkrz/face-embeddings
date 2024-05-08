820d4deed53d757c4cad501618414424287b80b2
Alexander Kurz <alex.kurz95@gmail.com>
2024-05-08 12:20:57 +0200

    Compute embeddings with official Arcface checkpoint

--- predict.py
+++ predict.py
@@ -1,11 +1,16 @@
 import os
 from collections import OrderedDict
 from pathlib import Path
+from typing import Optional
 
+import numpy as np
 import torch
 from jsonargparse import CLI
+from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 from src.backbones.build import build_backbone
+from src.datamodule import IMGFaceDataset
 
 
 def adjust_magface_dict(model: torch.nn.Module, state_dict: OrderedDict) -> OrderedDict:
@@ -21,22 +26,72 @@ def adjust_magface_dict(model: torch.nn.Module, state_dict: OrderedDict) -> Orde
 
 
 def main(
-    method: str = "magface",
-    ckpt_fp: str = "checkpoints/magface/magface_iresnet50_MS1MV2_ddp_fp32.pth",
+    data_dir: str,
+    output_dir: str,
+    file_ext: str = ".jpg",
+    data_name: Optional[str] = None,
+    model_name: str = "arcface",
+    ckpt_fp: str = "checkpoints/arcface/backbone_ms1mv3_arcface_r50_fp16.pth",
     backbone: str = "iresnet50",
+    gpu: Optional[int] = None,
 ):
+    data_dir = Path(data_dir)
+    output_dir = Path(output_dir)
+    assert data_dir.exists()
+    assert output_dir.exists()
+    if data_name is None:
+        data_name = data_dir.name
+
+    # Assign device where code is executed
+    if gpu is not None:
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)  # Only show pytorch the selected GPU
+        device = torch.device("cuda:0")
+    else:
+        device = torch.device("cpu")  # Use CPU
+
+    # Construct dataset
+    print(f"Generating dataset class for {data_name}..")
+    dataset = IMGFaceDataset(data_dir, file_ext, img_shape=(112, 112))
+    dataloader = DataLoader(dataset, batch_size=64, shuffle=False, num_workers=8)
+
+    print(f"Loading model checkpoint for {model_name}..")
     model = build_backbone(backbone=backbone, embed_dim=512, pretrained=False)
-    if method == "arcface":
+    if model_name == "arcface":
         state_dict = torch.load(ckpt_fp)
         # print(state_dict.keys())
         model.load_state_dict(state_dict)
-    elif method == "magface":
+    elif model_name == "magface":
         ckpt = torch.load(ckpt_fp)
         state_dict = adjust_magface_dict(model, ckpt["state_dict"])
         # print(state_dict.keys())
         model.load_state_dict(state_dict)
     else:
         raise NotImplementedError()
+
+    model.to(device)
+    model.eval()
+
+    filename_list = []
+    feats_list = []
+    print("Computing embeddings..")
+    for batch_data in tqdm(dataloader):
+        imgs, filenames = batch_data
+        with torch.no_grad():
+            feats = model(imgs.to(device))
+        feats = feats.cpu().numpy()  # Move feats to CPU and convert to numpy array
+        filename_list += filenames
+        feats_list.append(feats)
+
+    print("Saving results..")
+    feats_arr = np.concatenate(feats_list, axis=0)
+    print("Shape of feats_arr:", feats_arr.shape)
+    print("Dtype of feats_arr:", feats_arr.dtype)
+    # Save feats as .npy file
+    np.save(output_dir / f"{data_name}_{model_name}.npy", feats_arr)
+    # Save filenames as .txt file
+    with open(output_dir / f"{data_name}.txt", "w") as outfile:
+        outfile.write("\n".join(filename_list))
+        outfile.write("\n")  # Add final newline
 
 
 if __name__ == "__main__":
--- src/datamodule.py
+++ src/datamodule.py
@@ -1,11 +1,13 @@
 import numbers
 import os
-from typing import Optional
+from pathlib import Path
+from typing import Optional, Tuple
 
 import mxnet as mx
 import numpy as np
 import pytorch_lightning as pl
 import torch
+from PIL import Image
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 
@@ -87,3 +89,42 @@ class MXFaceDatamodule(pl.LightningDataModule):
             num_workers=self.hparams.num_workers,
             shuffle=True,
         )
+
+
+class IMGFaceDataset(Dataset):
+    default_transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+        ]
+    )
+
+    def __init__(
+        self,
+        root_dir: Path,
+        file_ext: str = ".jpg",
+        img_shape: Tuple[int, int] = (112, 112),
+        transform: Optional[transforms.Compose] = default_transform,
+    ):
+        super().__init__()
+        self.root_dir = root_dir
+        self.file_list = sorted(list(root_dir.rglob("*" + file_ext)))
+        self.img_shape = img_shape
+        self.resize_transform = transforms.Resize(self.img_shape)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.file_list)
+
+    def __getitem__(self, idx):
+        file_p = Path(self.file_list[idx])
+        relative_path = file_p.relative_to(self.root_dir)
+        img = Image.open(file_p)
+        # Check image shape and use resize transform if necessary
+        if not img.size == self.img_shape:
+            print("Rezising image")
+            img = self.resize_transform(img)
+        # Apply additional transforms
+        if self.transform:
+            img = self.transform(img)
+        return img, str(relative_path)
